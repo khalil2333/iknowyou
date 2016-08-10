049b55bc49a9b4f7012e5d38efbe390143e8420f
adam.d <adamchen.pub@gmail.com>
2016-08-10 21:21:52 +0800

    fit

---
+++ inu-logic/src/test/java/com/webank/inu/thirdService/TestSentiment.java
@@ -0,0 +1,14 @@
+package com.webank.inu.thirdService;
+
+import org.junit.Test;
+
+/**
+ * Created by adam on 2016/8/10.
+ */
+public class TestSentiment {
+
+    @Test
+    public void testSentiment(){
+
+    }
+}
