9508c6b580454d99e0675d06ff6a808b2a610f0a
adam.d <adamchen.pub@gmail.com>
2016-08-11 23:22:14 +0800

    a

--- inu-route/src/main/java/com/webank/inu/test/WechatProcess.java
+++ inu-route/src/main/java/com/webank/inu/test/WechatProcess.java
@@ -1,6 +1,10 @@
 package com.webank.inu.test;
 
-/** 
+import com.webank.inu.logic.service.message.IMessageService;
+import com.webank.inu.logic.service.message.ResponseInfo;
+import com.webank.inu.logic.service.message.impl.BaseMessageServiceImpl;
+
+/**
  * 微信xml消息处理流程逻辑类 
  * @author pamchen-1 
  * 
@@ -24,13 +28,26 @@ public class WechatProcess {
           
         /** 此时，如果用户输入的是“你好”，在经过上面的过程之后，result为“你也好”类似的内容  
          *  因为最终回复给微信的也是xml格式的数据，所有需要将其封装为文本类型返回消息 
-         * */  
+         * */
+        IMessageService messageService = new BaseMessageServiceImpl();
+        ResponseInfo responseInfo =
+                messageService.processMessage(xmlEntity.getFromUserName(), result,
+                        IMessageService.ResponseType.news);
+        //测试数据
         String title="test";
         String description="d";
         String picUrl="http://pic.sc.chinaz.com/files/pic/pic9/201508/apic14052.jpg";
         String url="http://docs.spring.io/spring-batch/reference/html/";
-        result = new FormatXmlProcess().formatXmlNewsAnswer(xmlEntity.getFromUserName(), xmlEntity.getToUserName(), result,
-        		title,description,picUrl,url);  
+        responseInfo.setTitle(title);
+        responseInfo.setDescription(description);
+        responseInfo.setPicUrl(picUrl);
+        responseInfo.setUrl(url);
+        //测试数据
+
+        result = new FormatXmlProcess().formatXmlNewsAnswer(
+                responseInfo.getToUserName(), xmlEntity.getToUserName(), responseInfo.getContent(),
+        		responseInfo.getTitle(),responseInfo.getDescription(),
+                responseInfo.getPicUrl(),responseInfo.getUrl());
           
         return result;  
     }  
