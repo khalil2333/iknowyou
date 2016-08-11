a9ab226f998a2325758dad789618ce9d1d776c21
adam.d <adamchen.pub@gmail.com>
2016-08-11 12:16:55 +0800

    history chat infos

---
+++ inu-logic/src/main/java/com/webank/inu/service/history/ChatInfo.java
@@ -0,0 +1,27 @@
+package com.webank.inu.service.history;
+
+/**
+ * Created by adam on 2016/8/11.
+ */
+public class ChatInfo {
+
+    public String content;
+
+    public long timestamp;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
